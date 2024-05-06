f5cd9103648666c6913524289a3a4fa3305b4e15
Sam <samllama55@gmail.com>
2024-05-06 19:02:37 +0200

    ADD: Simple let caching with clientLoader hydrate.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
-import { SerializeFrom, json, type MetaFunction } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { SerializeFrom, json, redirect, type MetaFunction } from '@remix-run/node';
+import { ClientLoaderFunctionArgs, Form, useLoaderData } from '@remix-run/react';
 import { db } from '~/db.server';
 import { placeholderImageUrl } from './_placeholderImage';
 
@@ -7,7 +7,9 @@ export const meta: MetaFunction = () => {
   return [{ title: 'Movie App' }, { name: 'description', content: 'Showing off the power of remix with a movie app!' }];
 };
 
-export const loader = async () => {
+let moviesCache: null | Awaited<ReturnType<typeof getRandomMovies>> = null;
+
+async function getRandomMovies() {
   const allIds = await db.movies.findMany({
     select: {
       id: true,
@@ -44,7 +46,32 @@ export const loader = async () => {
     },
     take: movieCount,
   });
+
+  return movies;
+}
+
+export const loader = async () => {
+  const movies = await getRandomMovies();
+
   return json({ movies });
+};
+
+export const clientLoader = async ({ serverLoader }: ClientLoaderFunctionArgs) => {
+  if (moviesCache !== null) {
+    return json({ movies: moviesCache });
+  }
+
+  const { movies } = await serverLoader<typeof loader>();
+
+  moviesCache = movies;
+
+  return json({ movies });
+};
+
+clientLoader.hydrate = true;
+
+export const action = async () => {
+  return redirect('/');
 };
 
 export default function Index() {
@@ -53,6 +80,10 @@ export default function Index() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-center">Movies app!</h1>
+
+        <Form method="post" className='flex justify-center my-4'>
+          <button className='p-2 bg-teal-200 text-slate-600 rounded' type="submit">Randomize!</button>
+        </Form>
 
       <div className="flex flex-wrap gap-2 p-4 justify-evenly">
         {movies.map((movie) => (
