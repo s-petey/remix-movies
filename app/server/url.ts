4e7674f35603bdbcbffbe1ac4b7e8be8ef7b347a
Sam <samllama55@gmail.com>
2024-05-09 08:56:09 +0200

    CHANGE: Remove server folder. CHANGE: Simplify vite config.

--- app/server/url.ts
+++
@@ -1,9 +0,0 @@
-let DATABASE_URL: string;
-
-if (typeof import.meta.env.VITE_DATABASE_URL === 'string') {
-  DATABASE_URL = import.meta.env.VITE_DATABASE_URL;
-} else {
-  throw new Error('Missing env');
-}
-
-export { DATABASE_URL };
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { installGlobals } from '@remix-run/node';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 installGlobals();
 
-export default defineConfig(({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
+export default defineConfig(() => {
   return {
     plugins: [remix(), tsconfigPaths()],
   };
