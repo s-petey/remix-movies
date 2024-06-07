f37c7cf16971aefb0e75424fd84bd438783c7721
Sam Peterson <sam@growblocks.com>
2024-06-07 17:37:36 +0200

    CHANGE: Add remix-flat-routes.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,23 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { installGlobals } from '@remix-run/node';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { flatRoutes } from 'remix-flat-routes';
 
 installGlobals();
 
 export default defineConfig(() => {
   return {
-    plugins: [remix(), tsconfigPaths()],
+    plugins: [
+      tsconfigPaths(),
+      remix({
+        ignoredRouteFiles: ['**/*'],
+        serverModuleFormat: 'esm',
+        routes: async (defineRoutes) => {
+          return flatRoutes('routes', defineRoutes, {
+            ignoredRouteFiles: ['**/*.test.{js,jsx,ts,tsx}', '**/__*.*'],
+          });
+        },
+      }),
+    ],
   };
 });
